48663a41a5ec5ba010037f8be39331ce42dbe1ea
MikePlante1 <82073483+MikePlante1@users.noreply.github.com>
2023-01-15 18:31:20 -0500

    Update BuildLoop.sh

--- BuildLoop.sh
+++ BuildLoop.sh
@@ -95,7 +95,7 @@ if [ "$WHICH" = "Loop" ]; then
     echo -e "Please select which version of Loop you would like to download and build.\n"
     echo -e "\n ${RED}${BOLD}You are running the script for the released version${NC}\n"
     echo -e "  These webpages will tell you the date of the last release for:"
-    echo -e "  Loop:    https://github.com/LoopKit/Loop/releases"
+    echo -e "  Loop:    https://github.com/MikePlante1/Loop/releases"
     echo -e "  FreeAPS: https://github.com/loopnlearn/LoopWorkspace/releases"
     if [ ${SCRIPT_BRANCH} == 'dev' ]; then
         echo -e "\n ${RED}${BOLD}This is the dev branch of BuildLoop.sh,"
@@ -116,7 +116,7 @@ if [ "$WHICH" = "Loop" ]; then
         case $opt in
             "Loop")
                 FORK_NAME=Loop
-                REPO=https://github.com/LoopKit/LoopWorkspace
+                REPO=https://github.com/MikePlante1/LoopWorkspace
                 BRANCH=$BRANCH_LOOP
                 break
                 ;;
